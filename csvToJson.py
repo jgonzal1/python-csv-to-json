0b0e01062e50534dec47cd96967ab95d317a666e
Javier <javier.gonzalezberenguel@gmail.com>
2023-03-23 19:58:53 +0100

    updated file for following iso standards

--- csvToJson.py
+++ csvToJson.py
@@ -1,5 +1,5 @@
 """
-module that transforms a CSV to JSON
+Module that transforms a CSV to JSON
 """
 import csv
 import json
@@ -7,22 +7,22 @@ import sys
 from collections import defaultdict
 
 '''
-main function which takes 2 col length CSVs
+Main function which takes 2 col length CSVs
 '''
 def transform(argv):
-  csvfile = open(argv[0],'r')
-  fichJSON = open(argv[1],'w')
+  csv_file_input = open(argv[0], 'r')
+  json_file_output = open(argv[1], 'w')
   
-  fieldnames = ("Col1", "Col2")
-  reader = csv.DictReader(csvfile, fieldnames) # assigned the json keys with fieldnames
+  column_key_names = ("Col1", "Col2")
+  reader = csv.DictReader(csv_file_input, column_key_names) # assigned the json keys with fieldnames
 
   output = []
   for each in reader:
       row = {}
-      for field in fieldnames:
+      for field in column_key_names:
           row[field] = each[field]
       output.append(row)       
-  json.dump(output, fichJSON, indent=3, sort_keys=True)
+  json.dump(output, json_file_output, indent=3, sort_keys=True)
   regroup = defaultdict(list)
 
 if __name__ == '__main__':
